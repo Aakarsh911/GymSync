cfe93c598052a8eb4e3fc57030bc5c60c44665c4
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-01-01 01:41:53 +0530

    Completed almost all of the functionality

--- backend/server.js
+++ backend/server.js
@@ -13,23 +13,23 @@ mongoose.connect('mongodb://0.0.0.0:27017/registeration'); // Replace with your 
 const db = mongoose.connection;
 
 const workoutSchema = new mongoose.Schema({
-  workoutName: String,
-  days: {
-    Monday: [],
-    Tuesday: [],
-    Wednesday: [],
-    Thursday: [],
-    Friday: [],
-    Saturday: [],
-    Sunday: [],
-  },
+    workoutName: String,
+    days: {
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+        Saturday: [],
+        Sunday: [],
+    },
 });
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  workouts: [workoutSchema],
+    username: String,
+    email: String,
+    password: String,
+    workouts: [workoutSchema],
 });
 
 const User = mongoose.model('User', userSchema);
@@ -39,122 +39,200 @@ const PORT = 3001;
 app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 app.post('/register', async (req, res) => {
-  try {
-    const existingUser = await User.findOne({ email: req.body.email });
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
 
-    if (existingUser) {
-      return res.status(409).json({ error: 'Email already exists. Please login.' });
+        if (existingUser) {
+            return res.status(409).json({ error: 'Email already exists. Please login.' });
+        }
+
+        const user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            workouts: [],
+        });
+        await user.save();
+        res.json({ message: 'Registration successful' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
     }
-
-    const user = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      workouts: [],
-    });
-    await user.save();
-    res.json({ message: 'Registration successful' });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
 });
 
 app.post('/login', async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
+    try {
+        const user = await User.findOne({ email: req.body.email });
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    if (user.password !== req.body.password) {
-      return res.status(401).json({ error: 'Incorrect password' });
-    }
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        if (user.password !== req.body.password) {
+            return res.status(401).json({ error: 'Incorrect password' });
+        }
 
-    // Successful login
-    res.json({ message: 'Login successful', username: user.username });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+        // Successful login
+        res.json({ message: 'Login successful', username: user.username });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.post('/addWorkout', async (req, res) => {
-  try {
-    const { username, workoutName } = req.body;
+    try {
+        const { username, workoutName } = req.body;
 
-    // Find the user by username
-    const user = await User.findOne({ username });
+        // Find the user by username
+        const user = await User.findOne({ username });
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Create a new workout object with the specified workout name and days
+        const newWorkout = {
+            workoutName,
+            days: {
+                Monday: [],
+                Tuesday: [],
+                Wednesday: [],
+                Thursday: [],
+                Friday: [],
+                Saturday: [],
+                Sunday: [],
+            },
+        };
+
+        // Push the new workout object to the workouts array
+        user.workouts.push(newWorkout);
+
+        // Save the updated user
+        await user.save();
+
+        res.json({ message: 'Workout added successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    // Create a new workout object with the specified workout name and days
-    const newWorkout = {
-      workoutName,
-      days: {
-        Monday: [],
-        Tuesday: [],
-        Wednesday: [],
-        Thursday: [],
-        Friday: [],
-        Saturday: [],
-        Sunday: [],
-      },
-    };
-
-    // Push the new workout object to the workouts array
-    user.workouts.push(newWorkout);
-
-    // Save the updated user
-    await user.save();
-
-    res.json({ message: 'Workout added successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
 });
 
 app.post('/deleteWorkout', async (req, res) => {
-  try {
-    const { username, workoutIndex } = req.body;
+    try {
+        const { username, workoutIndex } = req.body;
 
-    // Find the user by username
-    const user = await User.findOne({ username });
+        // Find the user by username
+        const user = await User.findOne({ username });
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Remove the workout at the specified index
+        user.workouts.splice(workoutIndex, 1);
+
+        // Save the updated user
+        await user.save();
+
+        res.json({ message: 'Workout deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    // Remove the workout at the specified index
-    user.workouts.splice(workoutIndex, 1);
-
-    // Save the updated user
-    await user.save();
-
-    res.json({ message: 'Workout deleted successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
 });
 
 app.get('/getWorkouts/:username', async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.params.username });
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
+    try {
+        const user = await User.findOne({ username: req.params.username });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
 
-    res.json({ workouts: user.workouts });
-  } catch (error) {
-    console.error("Error fetching workouts:", error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+        res.json({ workouts: user.workouts });
+    } catch (error) {
+        console.error("Error fetching workouts:", error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
+
+
+app.post('/updateWorkoutName', async (req, res) => {
+    try {
+        const { username, workoutIndex, newName } = req.body;
+
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        user.workouts[workoutIndex].workoutName = newName;
+
+        await user.save();
+
+        res.json({ message: 'Workout name updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/addExercise', async (req, res) => {
+    try {
+        const { username, workoutIndex, day, exercise } = req.body;
+
+        // Find the user by username
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Find the workout by index
+        const workout = user.workouts[workoutIndex];
+
+        if (!workout) {
+            return res.status(404).json({ error: 'Workout not found' });
+        }
+
+        // Add the exercise to the specified day
+        workout.days[day].push(exercise);
+
+        // Save the updated user
+        await user.save();
+
+        res.json({ message: 'Exercise added successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/getExercises/:username/:workoutIndex/:day', async (req, res) => {
+    try {
+      const { username, workoutIndex, day } = req.params;
+  
+      const user = await User.findOne({ username });
+  
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      const workout = user.workouts[workoutIndex];
+  
+      if (!workout) {
+        return res.status(404).json({ error: 'Workout not found' });
+      }
+  
+      const exercises = workout.days[day] || [];
+  
+      res.json({ exercises });
+    } catch (error) {
+      console.error("Error fetching exercises:", error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from "react";
 import userLogo from "./user.png";
+import Exercise from "./Excercise";
 
 function Home() {
   const username = localStorage.getItem("username");
   const [showForm, setShowForm] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
   const [workoutName, setWorkoutName] = useState("");
+  const [editWorkoutName, setEditWorkoutName] = useState("");
   const [userWorkouts, setUserWorkouts] = useState([]);
   const [hoveredWorkoutIndex, setHoveredWorkoutIndex] = useState(null);
+  const [editIndex, setEditIndex] = useState(null);
+  const [selectedDay, setSelectedDay] = useState("Monday");
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const [selectedWorkoutIndex, setSelectedWorkoutIndex] = useState(0);
+  const [showExerciseForm, setShowExerciseForm] = useState(false);
+  const [exerciseName, setExerciseName] = useState("");
+  const [numberOfSets, setNumberOfSets] = useState("");
+  const [numberOfRepetitions, setNumberOfRepetitions] = useState("");
+  const [exerciseTime, setExerciseTime] = useState("");
+  const [exerciseWeight, setExerciseWeight] = useState("");
+  const [exercises, setExercises] = useState([]);
 
   useEffect(() => {
     const fetchUserWorkouts = async () => {
@@ -14,25 +29,49 @@ function Home() {
         const response = await fetch(`http://localhost:3001/getWorkouts/${username}`);
         if (response.ok) {
           const data = await response.json();
-          // Extract only the workout names from the fetched data
           setUserWorkouts(data.workouts || []);
         } else {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
       } catch (error) {
         console.error("Error fetching workouts:", error.message);
-        // Handle error as needed
       }
     };
-
     fetchUserWorkouts();
   }, [username]);
+
+  useEffect(() => {
+    const fetchUserExercises = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/getExercises/${username}/${selectedWorkoutIndex}/${selectedDay}`);
+        if (response.ok) {
+          const data = await response.json();
+          setExercises(data.exercises || []);
+        } else {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error("Error fetching exercises:", error.message);
+        setExercises([]);
+      }
+    };
+    fetchUserExercises();
+  }, [username, selectedWorkoutIndex, selectedDay]);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const selectDay = (day) => {
+    setSelectedDay(day);
+    setShowDropdown(false);
+  };
 
   const addWorkout = () => {
     setShowForm(true);
   };
 
-  const handleSubmit = async () => {
+  const handleCreateSubmit = async () => {
     try {
       const response = await fetch("http://localhost:3001/addWorkout", {
         method: "POST",
@@ -54,7 +93,36 @@ function Home() {
       setShowForm(false);
     } catch (error) {
       console.error("Error adding workout:", error.message);
-      // Handle error as needed
+    }
+  };
+
+  const handleEditSubmit = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/updateWorkoutName", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          workoutIndex: editIndex,
+          newName: editWorkoutName,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const updatedWorkouts = [...userWorkouts];
+      updatedWorkouts[editIndex].workoutName = editWorkoutName;
+      setUserWorkouts(updatedWorkouts);
+
+      setEditWorkoutName("");
+      setEditIndex(null);
+      setShowEditForm(false);
+    } catch (error) {
+      console.error("Error updating workout name:", error.message);
     }
   };
 
@@ -70,17 +138,114 @@ function Home() {
           workoutIndex: index,
         }),
       });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const updatedWorkouts = [...userWorkouts];
+      updatedWorkouts.splice(index, 1);
+      setUserWorkouts(updatedWorkouts);
+  
+      if (editIndex === index) {
+        setShowEditForm(false);
+        setEditIndex(null);
+        setEditWorkoutName("");
+      }
+  
+      // Reset selected workout index and exercises when the current workout is deleted
+      if (selectedWorkoutIndex === index) {
+        setSelectedWorkoutIndex(0); // Set to the first workout or another default index
+        const updatedExercises = await fetchUpdatedExercises();
+        setExercises(updatedExercises);
+      }
+    } catch (error) {
+      console.error("Error deleting workout:", error.message);
+    }
+  };
+
+  const handleEdit = async (index) => {
+    setEditIndex(index);
+    setEditWorkoutName(userWorkouts[index].workoutName);
+    setShowEditForm(true);
+  };
+
+  const handleCancel = () => {
+    setShowForm(false);
+    setShowEditForm(false);
+    setWorkoutName("");
+    setEditWorkoutName("");
+    setEditIndex(null);
+  };
+
+  const selectWorkout = (index) => {
+    setSelectedWorkoutIndex(index);
+  };
+
+  const displayExerciseForm = () => {
+    setShowExerciseForm(true);
+  };
+
+  const handleExerciseSubmit = async () => {
+    try {
+      // Validate exercise name before submitting
+      if (!exerciseName) {
+        // Show error message using the error class
+        console.error("Exercise name is required.");
+        return;
+      }
+
+      const response = await fetch("http://localhost:3001/addExercise", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          workoutIndex: selectedWorkoutIndex,
+          day: selectedDay,
+          exercise: {
+            name: exerciseName || "N/A",
+            sets: numberOfSets || "N/A",
+            repetitions: numberOfRepetitions || "N/A",
+            time: exerciseTime || "N/A",
+            weight: exerciseWeight || "N/A",
+          },
+        }),
+      });
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const updatedWorkouts = [...userWorkouts];
-      updatedWorkouts.splice(index, 1);
-      setUserWorkouts(updatedWorkouts);
+      // Fetch the updated exercises for the selected workout and day
+      const updatedExercises = await fetchUpdatedExercises();
+      setExercises(updatedExercises);
+
+      // Reset the exercise form state
+      setExerciseName("");
+      setNumberOfSets("");
+      setNumberOfRepetitions("");
+      setExerciseTime("");
+      setExerciseWeight("");
+      setShowExerciseForm(false);
     } catch (error) {
-      console.error("Error deleting workout:", error.message);
-      // Handle error as needed
+      console.error("Error adding exercise:", error.message);
+    }
+  };
+
+  const fetchUpdatedExercises = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/getExercises/${username}/${selectedWorkoutIndex}/${selectedDay}`);
+      if (response.ok) {
+        const data = await response.json();
+        return data.exercises || [];
+      } else {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error fetching exercises:", error.message);
+      return [];
     }
   };
 
@@ -88,6 +253,46 @@ function Home() {
     <div className="container">
       <div className="day-panel">
         <h1>Workouts</h1>
+        {userWorkouts.map((workout, index) => (
+          <div
+            key={index}
+            className={`workout-item ${hoveredWorkoutIndex === index ? "hovered" : ""} ${selectedWorkoutIndex === index ? "selected-workout" : ""}`}
+            onMouseEnter={() => setHoveredWorkoutIndex(index)}
+            onMouseLeave={() => setHoveredWorkoutIndex(null)}
+            onClick={() => selectWorkout(index)}
+          >
+            <h2>{workout.workoutName}</h2>
+            {hoveredWorkoutIndex === index && (
+              <div className="action-icons">
+                <span className="delete-icon" onClick={() => handleDelete(index)}>
+                  🗑️
+                </span>
+                <span className="delete-icon" onClick={() => handleEdit(index)}>
+                  ✏️
+                </span>
+              </div>
+            )}
+            {showEditForm && editIndex === index && (
+              <div className="workout-form">
+                <input
+                  type="text"
+                  placeholder="Enter new workout name"
+                  className="workout-name"
+                  value={editWorkoutName}
+                  onChange={(e) => setEditWorkoutName(e.target.value)}
+                />
+                <div className="form-buttons">
+                  <button onClick={handleEditSubmit} className="create-workout">
+                    Save
+                  </button>
+                  <button onClick={handleCancel} className="create-workout">
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
         <h3 className="add" onClick={addWorkout}>
           Add a new workout schedule <span className="add-button">+</span>
         </h3>
@@ -96,35 +301,114 @@ function Home() {
             <input
               type="text"
               placeholder="Enter workout name"
+              className="workout-name"
               value={workoutName}
               onChange={(e) => setWorkoutName(e.target.value)}
             />
-            <button onClick={handleSubmit}>Submit</button>
+            <div className="form-buttons">
+              <button onClick={handleCreateSubmit} className="create-workout">
+                Create
+              </button>
+              <button onClick={handleCancel} className="create-workout">
+                Cancel
+              </button>
+            </div>
           </div>
         )}
-        {userWorkouts.map((workout, index) => (
-          <div
-            key={index}
-            className={`workout-item ${hoveredWorkoutIndex === index ? "hovered" : ""}`}
-            onMouseEnter={() => setHoveredWorkoutIndex(index)}
-            onMouseLeave={() => setHoveredWorkoutIndex(null)}
-          >
-            <h2>{workout.workoutName}</h2>
-            {hoveredWorkoutIndex === index && (
-              <span className="delete-icon" onClick={() => handleDelete(index)}>
-                🗑️
-              </span>
-            )}
-          </div>
-        ))}
       </div>
       <div className="dashboard">
         <div className="dashboard-nav">
-          <h1>{username}'s Dashboard</h1>
+          <h1 className="username">{username}'s Dashboard</h1>
           <div className="account">
             <img src={userLogo} alt="user" />
           </div>
         </div>
+        {userWorkouts[selectedWorkoutIndex] && <div className="dropdown">
+          <div className={`select ${showDropdown ? "select-clicked" : ""}`} onClick={toggleDropdown}>
+            <span className={`selected ${showDropdown ? "" : ""}`}>{selectedDay}</span>
+            <div className={`caret ${showDropdown ? "caret-rotate" : ""}`}></div>
+          </div>
+          <ul className={`menu ${showDropdown ? "menu-open" : ""}`}>
+            {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map((day) => (
+              <li key={day} className={selectedDay === day ? "active" : ""} onClick={() => selectDay(day)}>
+                {day}
+              </li>
+            ))}
+          </ul>
+          <div className="addExcercise" onClick={displayExerciseForm}>
+            +
+          </div>
+        </div>}
+        {showExerciseForm && (
+          <div className="exercise-form">
+            <input
+              type="text"
+              placeholder="Exercise Name"
+              className="exercise-name"
+              value={exerciseName}
+              onChange={(e) => setExerciseName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Number of Sets"
+              className="number-of-sets"
+              value={numberOfSets}
+              onChange={(e) => setNumberOfSets(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Number of Repetitions"
+              className="number-of-repetitions"
+              value={numberOfRepetitions}
+              onChange={(e) => setNumberOfRepetitions(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Time"
+              className="exercise-time"
+              value={exerciseTime}
+              onChange={(e) => setExerciseTime(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Weight"
+              className="exercise-weight"
+              value={exerciseWeight}
+              onChange={(e) => setExerciseWeight(e.target.value)}
+            />
+            <div className="form-buttons">
+              <button onClick={handleExerciseSubmit} className="create-exercise">
+                Submit
+              </button>
+              <button onClick={() => setShowExerciseForm(false)} className="create-exercise">
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
+
+        {userWorkouts[selectedWorkoutIndex] ? (
+          <div className="exercises-list">
+            <h2>Exercises for {userWorkouts[selectedWorkoutIndex]?.workoutName} - {selectedDay}</h2>
+            <ul>
+              {exercises.map((exercise, index) => (
+                <li key={index} className="exercise-item">
+                  <Exercise
+                    exerciseName={exercise.name}
+                    sets={exercise.sets}
+                    reps={exercise.repetitions}
+                    time={exercise.time}
+                    weight={exercise.weight}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div className="fix-top-margin">
+            <h2>No workout selected</h2>
+          </div>
+        )}
       </div>
     </div>
   );
