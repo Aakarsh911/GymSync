29af2f00af23ade073b68255d2e2227f11d02f2b
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-01-03 04:49:02 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require("cors");
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://gymance.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
