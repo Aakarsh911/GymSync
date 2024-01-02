78ee1174f7a8ece54af8a22c9b663405d5cdf6f4
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-01-03 04:38:23 +0530

    check

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,10 @@ app.use(cors(
     }
 ));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is  running on port ${PORT}`);
 });
